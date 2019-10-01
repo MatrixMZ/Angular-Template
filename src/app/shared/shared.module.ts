eed4d6437c5c6f10763c2d1cb8d9d750e3e238e8
Mateusz Ziobrowski <matrix.ziobrowski@gmail.com>
2019-10-02 00:15:01 +0100

    module import fix

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
 
   ],
   imports: [
-    CommonModule
+    CommonModule, ReactiveFormsModule
   ],
   exports: [
-    CommonModule
+    CommonModule, ReactiveFormsModule
   ]
 })
 export class SharedModule { }
